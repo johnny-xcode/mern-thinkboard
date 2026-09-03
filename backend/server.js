10d423d40039a3f12c37552414938e9832e22986
jhontab <jhontab@gmail.com>
2026-09-03 22:23:08 +0800

    ch9

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,26 @@
-// import express from "express"
-
-const express = require("express");
+import express from "express"
+import notesRoutes from "./notes/notesRoutes.js";
 
 const app = express();
 
-app.get("/api/notes", (req, res) => {
-   res.send("you got 5 notes.");
-});
+app.use('/api/notes', require('./notes/notesRoutes'));
 
-app.listen(5001, () =>{
-   console.log("Server started on Port: 5001");
-});
+// app.get("/api/notes", (req, res) => {
+//    res.status(200).send("You got 30 notes");
+// });
+
+// app.post("/api/notes", (req, res) => {
+//    res.status(201).json({message: "Post created successfully"});
+// });
+
+// app.put("/api/notes/:id", (req, res) => {
+//    res.status(200).json({message: "Post updated successfully"});
+// });
+
+// app.delete("/api/notes/:id", (req, res) => {
+//    res.status(200).json({message: "Post deleted successfully"});        
+// });
+
+ app.listen(5001, () =>{
+    console.log("Server started on Port: 5001");
+ });
