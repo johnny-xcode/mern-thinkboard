d8dd7a8e569990471c86d01acbf1a32d9d425b00
jhontab <jhontab@gmail.com>
2026-09-03 16:51:19 +0800

    ch7

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,10 @@ const express = require("express");
 
 const app = express();
 
+app.get("/api/notes", (req, res) => {
+   res.send("you got 5 notes.");
+});
+
 app.listen(5001, () =>{
    console.log("Server started on Port: 5001");
 });
