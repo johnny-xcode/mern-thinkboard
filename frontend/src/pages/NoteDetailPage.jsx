034359d114d028c7ad34b3aaeed5f4fea6cd4d32
jhontab <jhontab@gmail.com>
2026-09-14 16:26:00 +0800

    NoteDetail

--- frontend/src/pages/NoteDetailPage.jsx
+++ frontend/src/pages/NoteDetailPage.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import { useEffect, useState } from "react"
+import toast from "react-hot-toast";
+import { useNavigate, useParams } from "react-router"
 
-function NoteDetailPage() {
-  return (
-    <div>NoteDetailPage</div>
-  )
-}
+const NoteDetailPage = () => {
 
-export default NoteDetailPage
+  const [note, setNote] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [saving, setSaving] = useState(false);
+
+  const navigate = useNavigate();
+
+  const {id} = useParams();
+
+  // console.log(id);
+
+  useEffect(() => {
+    const fetchNote = async() => {
+
+        try {
+           const res = await api.get(`/notes/${id}`)
+           setNote =  (res.data)
+        } catch (error) {
+           console,log("Error in fetching note ", error)
+           toast.error("Failed to fetch the note.")
+        } finally {
+          setLoading(false)
+        }
+    };
+
+    fetchNote();
+  }, [id]);
+
+  console.log("note");
+
+  return <div>NoteDetailPage<div/>;
+ 
+  
+};
+
+export default NoteDetailPage;
